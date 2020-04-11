cdef0d0e8f97f205ac715845233a2222fbd1f06b
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-11 15:14:40 -0300

    Criando componente About

--- src/components/globals/GlobalStyles.js
+++ src/components/globals/GlobalStyles.js
@@ -45,7 +45,7 @@ const Globals = createGlobalStyle`
   }
   p {
     line-height: 1.5;
-    margin: 0 0 1.5;
+    margin: 0 0 1.5rem;
   }
 `;
 
--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,15 +1,97 @@
 import React from "react";
+import styled from "styled-components";
+
 import Section from "../globals/Section";
 import Title from "../globals/Title";
+import Button from "../globals/Button";
+
+import aboutImg from "../../images/aboutBcg.jpeg";
+import {
+  setRem,
+  setBorder,
+  setCollor,
+  setLetterSpacing,
+  media,
+  setColor,
+} from "../../styles";
 
 const About = () => {
   return (
     <div>
       <Section>
-        <Title title="about us" />
+        <AboutCenter>
+          <div className="about-img">
+            <img src={aboutImg} alt="about us" />
+          </div>
+          <div className="about-info">
+            <Title title="about us" />
+            <p>
+              Exercitation quis cillum est sint magna sunt incididunt
+              consectetur proident ea reprehenderit. Cillum consectetur
+              exercitation quis quis mollit dolore do reprehenderit proident
+              sint. Sunt ad id aliquip occaecat cupidatat enim nulla
+              exercitation deserunt ex labore. Ipsum labore non nulla et aute
+              sit ad mollit culpa ea quis. Deserunt culpa ea laboris ut ullamco
+              enim id labore sit pariatur. Enim Lorem adipisicing nostrud aliqua
+              et ipsum. Eu quis ut deserunt eiusmod nisi sunt incididunt nisi in
+              esse sint reprehenderit ipsum. Magna dolor officia ex adipisicing.
+              Sunt laboris duis exercitation ut Lorem sit cupidatat ad.
+              Exercitation quis cillum est sint magna sunt incididunt
+              consectetur proident ea reprehenderit. Cillum consectetur
+              exercitation quis quis mollit dolore do reprehenderit proident
+              sint. Sunt ad id aliquip occaecat cupidatat enim nulla
+              exercitation deserunt ex labore. Ipsum labore non nulla et aute
+              sit ad mollit culpa ea quis. Deserunt culpa ea laboris ut ullamco
+              enim id labore sit pariatur. Enim Lorem adipisicing nostrud aliqua
+              et ipsum. Eu quis ut deserunt eiusmod nisi sunt incididunt nisi in
+              esse sint reprehenderit ipsum. Magna dolor officia ex adipisicing.
+              Sunt laboris duis exercitation ut Lorem sit cupidatat ad.
+            </p>
+            <Button>read more</Button>
+          </div>
+        </AboutCenter>
       </Section>
     </div>
   );
 };
+
+const AboutCenter = styled.div`
+  max-width: 90vh;
+  margin: 0 auto;
+  padding: 0 ${setRem(60)};
+
+  .about-img {
+    img {
+      width: 100%;
+      display: block;
+      ${setBorder({ width: setRem(6), color: setColor.primaryColor })}
+    }
+  }
+  .about-info {
+    margin: 30px auto 0 auto;
+
+    p {
+      ${setLetterSpacing(3)};
+    }
+  }
+
+  ${media.desktop`
+    max-width: 100vw;
+    display: grid;
+    gap: 20px;
+    gap: ${setRem(32)};
+    grid-template-columns: repeat(2, 1fr);
+    
+    .about-img {
+      align-self: center;
+    }
+    .about-info {
+      margin: 0;
+      p {
+        margin-top: 0;
+      }
+    }
+  `};
+`;
 
 export default About;
